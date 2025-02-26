9b8c785d710383f6701950ca4d05f42079d94a8c
Rashindra Yadav <yadavrashindra5@gmail.com>
2025-02-26 12:11:53 +0530

    remove commanline runnable

--- orm/src/main/java/com/orm/OrmApplication.java
+++ orm/src/main/java/com/orm/OrmApplication.java
@@ -4,14 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrmApplication implements Runnable{
+public class OrmApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrmApplication.class, args);
-	}
-
-	@Override
-	public void run() {
-		System.out.println("Hello world");
 	}
 }
